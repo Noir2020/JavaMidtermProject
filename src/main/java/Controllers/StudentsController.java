74f083310e9e08062fe863eeb4bdaf751d30cbff
mariyabazeleva <mbazeleva@yahoo.com>
2020-07-02 12:15:54 -0400

    Version1

--- src/main/java/Controllers/StudentsController.java
+++ src/main/java/Controllers/StudentsController.java
@@ -21,7 +21,6 @@ public class StudentsController {
     }
 
     public void enrollStudent(Student student) {
-        //Add all of the constraints
 
         if (Validate(student)) {
             enrolledStudents.put(student.getSTUDENT_ID(), student);
@@ -55,42 +54,15 @@ public class StudentsController {
     }
 
     private boolean Validate(Student student) {
-//        if(student.getStudentStatus().toLowerCase().equals("international student")){
-//            if (student.getCredits() >= 12 && student.getCredits() <= 21) {
-//                    System.out.println("You are successfully registered as 'International student'.");
-//                    return true;
-//            } else {
-//                System.out.println("Sorry, the numbers of credits for International students should be in between 12 and 21");
-//                return false;
-//            }
-//        }
-//        if(student.getStudentStatus().toLowerCase().equals("in state student")){
-//            if (student.getCredits() >= 3 && student.getCredits() <= 21) {
-//                System.out.println("You are successfully registered as 'In state student'.");
-//                return true;
-//            } else {
-//                System.out.println("Sorry, the numbers of credits for In state student should be in between 12 and 21");
-//                return false;
-//            }
-//        }
-//        if(student.getStudentStatus().toLowerCase().equals("out of state student")){
-//            if (student.getCredits() >= 3 && student.getCredits() <= 21) {
-//                System.out.println("You are successfully registered as 'Out state student'.");
-//                return true;
-//            } else {
-//                System.out.println("Sorry, the numbers of credits for Out state student should be in between 12 and 21");
-//                return false;
-//            }
-//        }
+
         if(student.getFirstName().toLowerCase().trim().equals("") ||
             student.getLastName().toLowerCase().trim().equals("") ||
             student.getDocumentNumber().toLowerCase().trim().equals("")||
-            student.getStudentStatus().toLowerCase().trim().equals("")
-        ){
+            student.getStudentStatus().toLowerCase().trim().equals(""))
+        {
             System.out.println("No fields can be empty");
             return false;
         }
-        System.out.println(student.getStudentStatus().toLowerCase().trim());
         if(student.getStudentStatus().toLowerCase().trim().equals("in state student")){
 
             System.out.println("You were successfully registered as In State Student");
